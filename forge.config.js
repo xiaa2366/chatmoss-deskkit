cc1fa4d788d8a7f9bc2392611d9f4fcc892ace3a
朱昆鹏 <270750933@qq.com>
2023-03-29 12:42:00 +0800

    feat: 初始化项目

---
+++ forge.config.js
@@ -0,0 +1,58 @@
+const { parsed } = require("dotenv").config();
+module.exports = {
+  packagerConfig: {
+    name: "ChatGPT",
+    executableName: "ChatGPT",
+    icon: "images/icon",
+    appBundleId: "com.vincelwt.chatgptmac",
+    extendInfo: {
+      LSUIElement: "true",
+    },
+    osxSign: {
+      hardenedRuntime: false,
+      gatekeeperAssess: false,
+      identity: "Developer ID Application: Lyser.io Ltd (R4PF6TTR6Z)",
+    },
+    osxNotarize: {
+      appBundleId: "com.vincelwt.chatgptmac",
+
+      tool: "notarytool",
+      appleId: 'img.zhu.electron',
+      appleIdPassword: 'test',
+      teamId: '13',
+    },
+  },
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "vincelwt",
+          name: "chatgpt-mac",
+        },
+        prerelease: true,
+      },
+    },
+  ],
+
+  rebuildConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-dmg",
+      platforms: ["darwin"],
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {},
+    },
+  ],
+};
